f406ad9ef9bf7d3342b6318984439f3058076943
Danny Cairns <dpcairns@gmail.com>
2020-01-28 14:07:33 -0800

    oops

---
+++ docs/precache-manifest.cc1fb10ccae1f1bd08ac068d943ce539.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634c06b923c73dbe72ba31f901bedff9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fe4ba294cecb6140401",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1371c143d0d135857376",
+    "url": "/static/js/2.f322d226.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f322d226.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3fe4ba294cecb6140401",
+    "url": "/static/js/main.3228c730.chunk.js"
+  },
+  {
+    "revision": "4f8b734a7e80109b6f7e",
+    "url": "/static/js/runtime-main.10a268e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
